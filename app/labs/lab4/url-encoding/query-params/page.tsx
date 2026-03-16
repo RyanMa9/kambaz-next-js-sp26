b74d92990ccb51f2757027296b799fdc128157d8
Ryan Ma <ryaenma@gmail.com>
2026-03-16 01:55:33 -0400

    fast

--- app/labs/lab4/url-encoding/query-params/page.tsx
+++ app/labs/lab4/url-encoding/query-params/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 export default function QueryCalculator() {
   const searchParams = useSearchParams();
   const aRaw = searchParams.get("a") || "0";
@@ -9,16 +10,18 @@ export default function QueryCalculator() {
   const b = parseFloat(bRaw);
   const sum = a + b;
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Calculator – Query Parameters</h1>
-      Raw query values (already decoded by Next.js):
-      <p>
-        a = <code>{aRaw}</code>
-      </p>
-      <p>
-        b = <code>{bRaw}</code>
-      </p>
-      <h2 style={{ color: "green" }}>Sum = {sum}</h2>
-    </div>
+    <Suspense>
+      <div style={{ padding: 40 }}>
+        <h1>Calculator – Query Parameters</h1>
+        Raw query values (already decoded by Next.js):
+        <p>
+          a = <code>{aRaw}</code>
+        </p>
+        <p>
+          b = <code>{bRaw}</code>
+        </p>
+        <h2 style={{ color: "green" }}>Sum = {sum}</h2>
+      </div>
+    </Suspense>
   );
 }
